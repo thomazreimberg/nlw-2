e9d96123342a6f9680a9a5038f08c532424a9466
Thomaz Henrique <treimberg7@gmail.com>
2020-08-03 12:29:47 -0300

    create react

---
+++ project/web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a>Hello</a>
+      <button>Clique em mim</button>
+    </div>
+  );
+}
+
+export default App;
